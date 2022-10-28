6da757bb34b69946205dc4f05d2cc9f2dd5769b4
Rafael Zasas <zasas.rafi@gmail.com>
2022-10-28 16:34:10 -0700

    WIP: Adding python linked list

--- python/data_structures/ll.py
+++ python/data_structures/ll.py
@@ -1,3 +1,12 @@
 #!/usr/local/bin/python3
+class Node:
+	def __init__(self, value=None):
+		self.value = value
+		self.next = None
 
-#TODO: Complete linked list implimentation for python
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+def add(l: LinkedList):
+	
